eb139a5a9082af30eed27133198facf9dd1367fd
Carlos Parra <carlos.parra.sbr@gmail.com>
2016-05-29 02:42:35 -0400

    Organizing all the files

--- WordPressAutomation/Navigation/LeftNavigation.cs
+++ WordPressAutomation/Navigation/LeftNavigation.cs
@@ -28,17 +28,5 @@ namespace WordPressAutomation
             }
         }
 
-        public class MenuSelector {
-
-            public static void Select(string topLevelMenuId) {
-                Driver.Instance.FindElement(By.Id(topLevelMenuId)).Click();
-            }
-            public static void Select(string topLevelMenuId, string subMenuLinkText)
-            {
-                Driver.Instance.FindElement(By.Id(topLevelMenuId)).Click();
-                Driver.Instance.FindElement(By.LinkText(subMenuLinkText)).Click();
-            }
-        }
-
     }
 }
---
+++ WordPressAutomation/Navigation/MenuSelector.cs
@@ -0,0 +1,22 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WordPressAutomation
+{
+    public class MenuSelector
+    {
+
+        public static void Select(string topLevelMenuId)
+        {
+            Driver.Instance.FindElement(By.Id(topLevelMenuId)).Click();
+        }
+        public static void Select(string topLevelMenuId, string subMenuLinkText)
+        {
+            Driver.Instance.FindElement(By.Id(topLevelMenuId)).Click();
+            Driver.Instance.FindElement(By.LinkText(subMenuLinkText)).Click();
+        }
+    }
+}
