495df199330accc01e4d93581b29f339903a379f
Carlos Parra <carlos.parra.sbr@gmail.com>
2016-05-29 01:03:00 -0400

    Creating new Base Class

---
+++ WordPressTests/BaseClass.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using WordPressAutomation;
+
+namespace WordPressTests
+{
+    public class BaseClass
+    {
+        [TestInitialize]
+        public void Init() {
+            Driver.Initialize();
+            #region login
+            LoginPage.GoTo();
+            LoginPage.LoginAs("user")
+                        .WithPassword("admintoor")
+                        .Login();
+            #endregion
+
+        }
+
+        [TestCleanup]
+        public void Cleanup() {
+            Driver.Close();
+        }
+    }
+}
